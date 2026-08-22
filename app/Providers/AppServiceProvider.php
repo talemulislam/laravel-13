c61d5d1ca1157b24222a19c4ea092cb92728b27f
Rahik Tahmid <rahiktahmid007@gmail.com>
2026-08-22 18:13:47 +0600

    bindif

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,11 +17,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-         $this->app->bind(Transistor::class, function (Application $app) {
+        //  $this->app->bind(Transistor::class, function (Application $app) {
+        //     return new Transistor(
+        //         $app->make(PodcastParser::class)
+        //     );
+        // });
+        $this->app->bindIf(Transistor::class, function (Application $app) {
             return new Transistor(
                 $app->make(PodcastParser::class)
             );
         });
+       
     }
 
     /*
